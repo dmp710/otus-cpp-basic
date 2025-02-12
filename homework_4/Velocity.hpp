fb0f2a1c5d73c5aabcd74fd8d523bdf3611c51df
dmitrii <dpipekin@elveesneotek.com>
2025-02-12 22:22:10 +0300

    homework 4 fix

--- homework_4/Velocity.hpp
+++ homework_4/Velocity.hpp
@@ -6,17 +6,18 @@ class Velocity {
   public:
     inline Velocity() = default;
 
-    // TODO: комментарии
+    // конструктор использует угол в радианах и абсолютное значение скорости, чтобы вычислить компоненты скорости по осям x и y
     inline Velocity(double abs, double angle) {
         const double x = std::cos(angle);
         const double y = std::sin(angle);
         vec = Point{x, y} * abs;
     }
-
+    // конструктор просто передает вектор в метод setVector для инициализации
     inline Velocity(const Point& vector) {
         setVector(vector);
     }
 
+    // устанавливает внутреннюю переменную вектор скорости
     inline void setVector(const Point& vector) {
         vec = vector;
     }
